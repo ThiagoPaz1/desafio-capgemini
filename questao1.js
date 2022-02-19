e641689892ce0b7b96d38766ed391ec208a67c5c
Thiago da Paz <thiagopazjunior@gmail.com>
2022-02-19 13:08:27 -0300

    Adicionando testes da questao 1.

--- questao1.js
+++ questao1.js
@@ -1,10 +1,21 @@
+let character = '';
+let heigth = '';
+
 const ladder = (n) => {
-  let character = '';
   let space = ' ';
 
   for (let i = 1; i <= n; i++) {
+    heigth += '*'
     console.log(`${space.repeat(n-i)}${character += '*'}`);
   }
+
+  return n
 }
 
 ladder(6);
+
+module.exports = {
+  character,
+  heigth,
+  ladder,
+}
